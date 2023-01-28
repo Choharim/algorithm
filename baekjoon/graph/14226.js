6f0117fcfbea3a8fd8403115c5339d46eeaaf858
Choharim <73021666+Choharim@users.noreply.github.com>
2023-01-28 22:51:10 +0900

    baekjoon bfs 복습 7569, 14226

--- baekjoon/graph/14226.js
+++ baekjoon/graph/14226.js
@@ -3,62 +3,32 @@ const count = Number(input);
 const queue = [];
 const check = {};
 
-queue.push([1, 0, 0]); // 화면에 있는 갯수, 클립보드에 복사된 갯수, 연산을 사용한 횟수
-check[`${1}_${0}`] = 1;
+queue.push([1, 0, 0]); // 화면에 있는 갯수, 클립보드에 복사된 갯수, 걸린 시간
+check[`1,0`] = 1; // 화면에 있는 갯수, 클립보드에 복사된 갯수의 상황 체크
 
 let calculation;
 while (queue.length) {
-  [screenCount, copedCount, lv] = queue.shift();
+  [screen, copied, time] = queue.shift();
 
-  calculation = [[screenCount, screenCount]];
-
-  if (copedCount > 0) calculation.push([screenCount + copedCount, copedCount]);
-  if (screenCount > 0) calculation.push([screenCount - 1, copedCount]);
+  calculation = [
+    [screen, screen],
+    [screen + copied, copied],
+    [screen - 1, copied],
+  ];
 
   for (let i = 0; i < calculation.length; i++) {
-    if (check[`${calculation[i][0]}_${calculation[i][1]}`]) continue;
-    if (calculation[i][0] === count) {
-      console.log(lv + 1);
+    [nextScreen, nextCopied] = calculation[i];
+
+    if (check[`${nextScreen},${nextCopied}`]) continue; // 이전에 확인한 상황은 넘어갑니다.
+    // nextScreen,nextCopied이 모두 0일 때는 다음에 할 수 있는 연산이 없기 때문에 넘어갑니다.
+    // nextScreen가 음수일 경우 nextScreen가 0일 때 삭제 연산을 사용한 것이므로 넘어갑니다.
+    if ((!nextScreen && !nextCopied) || nextScreen < 0) continue;
+
+    if (nextScreen === count) {
+      console.log(time + 1);
       return;
     }
-
-    queue.push([calculation[i][0], calculation[i][1], lv + 1]);
-    check[`${calculation[i][0]}_${calculation[i][1]}`] = 1;
+    queue.push([nextScreen, nextCopied, time + 1]);
+    check[`${nextScreen},${nextCopied}`] = 1;
   }
 }
-
-// while (queue.length) {
-//     [screenCount, copedCount, lv] = queue.shift();
-
-//     // 1. 연산
-//     if (!check[`${screenCount}_${screenCount}`]) {
-//       if (screenCount === count) {
-//         level = lv + 1;
-//         break;
-//       }
-//       queue.push([screenCount, screenCount, lv + 1]);
-//       check[`${screenCount}_${screenCount}`] = 1;
-//     }
-
-//     // 2. 연산
-//     if (copedCount > 0 && !check[`${screenCount + copedCount}_${copedCount}`]) {
-//       if (screenCount + copedCount === count) {
-//         level = lv + 1;
-//         break;
-//       }
-//       queue.push([screenCount + copedCount, copedCount, lv + 1]);
-//       check[`${screenCount + copedCount}_${copedCount}`] = 1;
-//     }
-
-//     // 3. 연산
-//     if (screenCount > 0 && !check[`${screenCount - 1}_${copedCount}`]) {
-//       if (screenCount - 1 === count) {
-//         level = lv + 1;
-//         break;
-//       }
-//       queue.push([screenCount - 1, copedCount, lv + 1]);
-//       check[`${screenCount - 1}_${copedCount}`] = 1;
-//     }
-//   }
-
-//   console.log(level);
--- baekjoon/graph/7569.js
+++ baekjoon/graph/7569.js
@@ -1,52 +1,55 @@
 const [M, N, H] = input[0].split(" ").map(Number);
 
 const box = Array.from({ length: N }, () =>
-  Array.from({ length: M }, () => [])
+  Array.from({ length: M }, () => Array(H))
 );
-let total = N * M * H;
+
+let total = M * N * H;
 const queue = [];
 
-for (let x = 0; x < N; ++x) {
-  for (let z = 0; z < H; ++z) {
-    const row = input[x + 1 + N * z].split(" ");
-    for (let y = 0; y < M; ++y) {
-      box[x][y][z] = +row[y];
+for (let n = 1; n <= N; n++) {
+  for (let h = 0; h < H; h++) {
+    const row = input[n + N * h].split(" ").map(Number);
 
-      if (row[y] < 0) {
-        total--;
-      }
-      if (row[y] == 1) {
-        total--;
-        queue.push([x, y, z, 0]);
+    for (let m = 0; m < M; m++) {
+      box[n - 1][m][h] = row[m];
+
+      if (row[m] === 1) {
+        queue.push([n - 1, m, h]);
+        total--; // 익은 토마토 갯수
+      } else if (row[m] === -1) {
+        total--; // 빈 곳 갯수
       }
     }
   }
 }
 
-const dx = [0, 0, 0, 0, -1, 1];
-const dy = [0, 0, -1, 1, 0, 0];
-const dz = [-1, 1, 0, 0, 0, 0];
-let queueIdx = 0;
-let day = 0;
+let days = 0;
 
-while (queueIdx < queue.length) {
-  [x, y, z, d] = queue[queueIdx];
-  queueIdx++;
+let idx = 0;
+const dx = [0, 1, 0, -1, 0, 0];
+const dy = [1, 0, -1, 0, 0, 0];
+const dz = [0, 0, 0, 0, 1, -1];
 
-  day = d + 1;
+while (idx < queue.length) {
+  [x, y, z] = queue[idx];
+  idx++;
 
   for (let i = 0; i < 6; i++) {
     const cx = x + dx[i];
     const cy = y + dy[i];
     const cz = z + dz[i];
-
     if (cx < 0 || cx >= N || cy < 0 || cy >= M || cz < 0 || cz >= H) continue;
     if (box[cx][cy][cz] === 0) {
-      total--;
-      box[cx][cy][cz] = day;
-      queue.push([cx, cy, cz, day]);
+      total--; // 익혀진 갯수
+
+      queue.push([cx, cy, cz]);
+      box[cx][cy][cz] = box[x][y][z] + 1;
+    }
+    if (idx === queue.length) {
+      days = box[x][y][z] - 1;
     }
   }
 }
 
-console.log(total ? -1 : day - 1);
+console.log(total ? -1 : days);
