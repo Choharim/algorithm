a1511cc2dd2981367dbd70394762f5883fc3ef7e
Choharim <jhl8586@naver.com>
2023-12-28 07:51:57 +0900

    input template 추가

--- baekjoon/index.js
+++ baekjoon/index.js
@@ -1,11 +1,6 @@
-// const { match } = require("assert");
-
-const { unwatchFile } = require("fs");
-const { off } = require("process");
-
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 const input = require("fs")
-  .readFileSync(filePath)
+  .readFileSync(__dirname + filePath)
   .toString()
   .trim()
   .split("\n");
--- baekjoon/template.js
+++ baekjoon/template.js
@@ -1,18 +1,20 @@
-// const input = require("fs").readFileSync("/dev/stdin").toString().trim();
-// const input = require("fs").readFileSync("./input.txt").toString().trim();
-
-// const input = require("fs")
-//   .readFileSync("/dev/stdin")
+// input 1줄
+// let input = require("fs")
+//   .readFileSync(__dirname + "/input.txt")
 //   .toString()
-//   .trim()
-//   .split("\n");
+//   .trim();
+// console.log(input);
 
-// const input = require("fs")
-//   .readFileSync("./input.txt")
-//   .toString()
-//   .trim()
-//   .split("\n");
+// input 1줄 이상
+let input = require("fs")
+  .readFileSync(__dirname + "/input.txt")
+  .toString()
+  .trim()
+  .split("\n");
+input = input.map((i) => i.split(" ").map(Number));
+console.log(input);
 
+// 위의 input이 안될 때
 // const readline = require("readline");
 // const rl = readline.createInterface({
 //   input: process.stdin,
